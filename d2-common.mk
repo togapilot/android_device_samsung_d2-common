6cc5ce222df4ae01bde384e80f5df0ea2f76bbd2
Joelz9614 <joelz9614@gmail.com>
2012-12-05 23:30:17 -0700

    these are not working

--- d2-common.mk
+++ d2-common.mk
@@ -104,9 +104,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html \
     ro.com.google.clientidbase=android-google \
     ro.com.android.wifi-watchlist=GoogleGuest \
-    ro.setupwizard.enterprise_mode=1 \
-    ro.com.android.dateformat=MM-dd-yyyy \
-    ro.com.android.dataroaming=false
+    ro.setupwizard.enterprise_mode=1 
 
 # common msm8960
 $(call inherit-product, device/samsung/msm8960-common/msm8960.mk)
