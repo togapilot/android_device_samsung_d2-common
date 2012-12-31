04903dab8f8e3b5b542c283440add112416fd186
boss <boss@boss-Inspiron-1545.(none)>
2012-12-31 02:53:40 -0800

    add invisiblek samsung fix

--- d2-common.mk
+++ d2-common.mk
@@ -100,7 +100,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.url.legal.android_privacy=http://www.google.com/intl/%s/mobile/android/basic/privacy.html \
     ro.com.google.clientidbase=android-google \
     ro.com.android.wifi-watchlist=GoogleGuest \
-    ro.setupwizard.enterprise_mode=1 
+    ro.setupwizard.enterprise_mode=1 \
+    ro.ril.samsung_data_call_magic=true
 
 # Workaround for MDP underrun issue with 4-layer composition
 PRODUCT_PROPERTY_OVERRIDES += \
