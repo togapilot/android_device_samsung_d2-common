26be1c5dce19406a050c91dcf9db498835b83939
boss <boss@boss-Inspiron-1545.(none)>
2013-01-11 21:53:25 -0800

    5.1 sound support

--- d2-common.mk
+++ d2-common.mk
@@ -105,7 +105,8 @@ PRODUCT_PROPERTY_OVERRIDES += \
     ro.com.google.clientidbase=android-google \
     ro.com.android.wifi-watchlist=GoogleGuest \
     ro.setupwizard.enterprise_mode=1 \
-    ro.ril.samsung_data_call_magic=true
+    ro.ril.samsung_data_call_magic=true \
+    media.aac_51_output_enabled=true
 
 # common msm8960
 $(call inherit-product, device/samsung/msm8960-common/msm8960.mk)
